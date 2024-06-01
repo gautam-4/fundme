a25910ad75cf8fd6d421d664fb27c8dc9ee1be56
gautam-4 <gautamalt468@gmail.com>
2024-06-01 18:34:24 +0530

    fixes

--- client/src/components/Fund.jsx
+++ client/src/components/Fund.jsx
@@ -1,26 +1,55 @@
-import '../styles/fund.css'
-import { useContext } from 'react';
-import { FundMeContext } from '../context/FundMeContext';
+import React, { useContext } from "react";
+import { FundMeContext } from "../context/FundMeContext";
+import '../styles/fund.css';
 
 function Fund() {
-  const {fundCount, connectWallet,fundHistory,currentAccount,isLoading,sendFund,handleChange,formData} = useContext(FundMeContext);
-  return (
-    <>
-      <h1>Transfer funds instantly</h1>
-      <div className="flex-between">
-        <div>
-          <input type="number" onChange={() => { }} placeholder="Amount" step='0.0001'/> <br />
-          <input type="text" onChange={() => { }} placeholder="Message" />
-          <br />
-          <button onClick={connectWallet}>Connect Wallet</button>
-        </div>
+    const {
+        fundCount,
+        connectWallet,
+        fundHistory,
+        currentAccount,
+        isLoading,
+        sendFund,
+        handleChange,
+        formData,
+    } = useContext(FundMeContext);
 
-        <div className="ethCard">
+    return (
+        <>
+            <h1>Transfer funds instantly</h1>
+            <div className="flex-between">
+                <div>
+                    <input
+                        type="number"
+                        onChange={(e) => handleChange(e, "amount")}
+                        placeholder="Amount"
+                        step="0.0001"
+                        value={formData.amount}
+                    />{" "}
+                    <br />
+                    <input
+                        type="text"
+                        onChange={(e) => handleChange(e, "message")}
+                        placeholder="Message"
+                        value={formData.message}
+                    />
+                    <br />
+                    {!currentAccount ? (
+                        <button onClick={connectWallet}>Connect Wallet</button>
+                    ) : (
+                        <button onClick={sendFund} disabled={isLoading}>
+                            {isLoading ? "Sending..." : "Fund"}
+                        </button>
+                    )}
+                </div>
 
-        </div>
-      </div>
-    </>
-  )
+                <div className="ethCard">
+                  
+                </div>
+                    
+            </div>
+        </>
+    );
 }
 
-export default Fund
+export default Fund;
